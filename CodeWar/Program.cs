64e2346182b974955656e3997234d4581c19a92b
HOYIN CHAN <hychan1990@gmail.com>
2019-10-26 01:49:20 +0800

    validParentheses

--- CodeWar/Program.cs
+++ CodeWar/Program.cs
@@ -9,19 +9,14 @@ namespace CodeWar
     {
         static void Main(string[] args)
         {
-            AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
-            ListNode l1 = new ListNode(1);
-            ListNode l2 = new ListNode(8);
-            //ListNode l3 = new ListNode(3);
-            l1.next = l2;
-            //l2.next = l3;
-            ListNode l4 = new ListNode(0);
-            //ListNode l5 = new ListNode(7);
-            //ListNode l6 = new ListNode(9);
-            //l4.next = l5;
-            //l5.next = l6;
-
-            addTwoNumbers.AddTwoNumbersMethod(l1, l4);
+            ValidParentheses20 validParentheses20 = new ValidParentheses20();
+            Console.WriteLine(validParentheses20.IsValid("{}"));
+            Console.WriteLine(validParentheses20.IsValid("()[]{}"));
+            Console.WriteLine(validParentheses20.IsValid("(]"));
+            Console.WriteLine(validParentheses20.IsValid("([)]"));
+            Console.WriteLine(validParentheses20.IsValid("{[]}"));
+            Console.WriteLine(validParentheses20.IsValid(""));
+            Console.Read();
         }
     }
 }
---
+++ CodeWar/ValidParentheses20.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWar
+{
+    class ValidParentheses20
+    {
+        public bool IsValid(string s)
+        {
+            //when recorded a open, next one must be [other open] or [same close]
+            //if recorded a close before opened, false
+            while (s.Contains("()") || s.Contains("{}") || s.Contains("[]"))
+            {
+                s = s.Replace("()", "");
+                s = s.Replace("{}", "");
+                s = s.Replace("[]", "");
+            }
+            return s.Length == 0;
+            ////()
+            //List<char> open = new List<char>(new char[]{ '(', '{', '[' });
+            //List<char> close = new List<char>(new char[] { ')', '}', ']' });
+            //bool opened = false;
+            //bool closed = false;
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    if (close.Contains(s[i]) && !open.Contains(s[i - 1])) //close without open
+            //    {
+            //        return false;
+            //    }
+
+            //    if (open.Contains(s[i]))
+            //    {
+            //        opened = true;
+            //    }
+
+            //}
+
+        }
+    }
+}
